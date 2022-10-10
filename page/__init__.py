50729bc3364d49c2dbe4c2065ee8debe3340089a
chenxiaojie111 <1342915623@qq.com>
2022-10-10 21:54:23 +0800

    自动化测试框架中基于PO模式的代码结构

--- page/__init__.py
+++ page/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 # @Author     : Bonnie
-# @Time       : 2022/10/9 22:54
+# @Time       : 2022/10/10 17:13
 # @FileName   : __init__.py.py
 # @Description:
---
+++ testcases/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Author     : Bonnie
+# @Time       : 2022/10/10 17:14
+# @FileName   : __init__.py.py
+# @Description:
